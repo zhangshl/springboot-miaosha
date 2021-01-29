f36cb2752558e73b0d8943a2c7f8d5cd8fc67e0a
zhangshaolong001 <zhangshaolong001@ke.com>
2021-01-29 16:24:05 +0800

    finish function

--- springboot-miaosha-portal/src/main/java/com/simple/controller/OrderController.java
+++ springboot-miaosha-portal/src/main/java/com/simple/controller/OrderController.java
@@ -2,8 +2,10 @@ package com.simple.controller;
 
 import com.simple.constanst.Constants;
 import com.simple.domain.Order;
+import com.simple.domain.SkGoods;
 import com.simple.enums.RocketMQDelayLevelEnum;
 import com.simple.service.OrderService;
+import org.apache.ibatis.annotations.Param;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
@@ -64,6 +66,8 @@ public class OrderController {
             return null;
         }
 
+        //TODO 分库分表的情况下，订单ID尾号后几位应为userId后几位，方便C端用户查询自己订单数据，至于用几位则根据分了多少张表而定，商家通过es查询
+        //同时可将数据同步到redis和es一份
         order.setId(System.currentTimeMillis() + orderIdGenerate.incrementAndGet());
         /**发送延时消息，预扣了库存，如果提单失败，或者到期不支付，则回滚预扣的库存*/
         SendResult sendResult = rocketMQTemplate.syncSend(delayTopic, MessageBuilder.withPayload(order).build(), 1000, RocketMQDelayLevelEnum.DELAY_5M.getLevel());
@@ -82,5 +86,18 @@ public class OrderController {
 
         //返回订单信息，跳转支付页面
         return order;
+    }
+
+
+    /**
+     * 查询订单
+     * @param orderId
+     * @return
+     */
+    @RequestMapping("/queryOrder")
+    @ResponseBody
+    public Order queryProduct(@Param("orderId") Long orderId) {
+        //TODO 实际场景会涉及redis和elasticsearch，性能和分库分表的关系
+        return orderService.selectByPrimaryKey(orderId);
     }
 }
